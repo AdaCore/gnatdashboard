7bea66cd49bf705680a6d5ee93d837cc69b56e3f
martin <martin@936e1b1b-40f2-da11-902a-00137254ae57>
2012-07-11 15:09:53 +0000

    sgCommit style check test
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/qualimetrics@192103 936e1b1b-40f2-da11-902a-00137254ae57

--- prototype/ada/src/main/java/org/sonar/plugins/ada/AdaSourceImporter.java
+++ prototype/ada/src/main/java/org/sonar/plugins/ada/AdaSourceImporter.java
@@ -1,12 +1,15 @@
 /**
  *
  * Sonar Ada Plugin
- * Copyright AdaCore, 2012
+ * Copyright (C) 2012, AdaCore
  */
 package org.sonar.plugins.ada;
 
 import org.sonar.api.batch.AbstractSourceImporter;
+import org.sonar.api.batch.Phase;
+import org.sonar.api.batch.ResourceCreationLock;
 
+@Phase(name = Phase.Name.PRE)
 public class AdaSourceImporter extends AbstractSourceImporter {
 
     public AdaSourceImporter(Ada lang) {
