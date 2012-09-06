816c35f6e2f3aa017c2df51c98ea1c8625370ec3
martin <martin@936e1b1b-40f2-da11-902a-00137254ae57>
2012-09-06 15:48:10 +0000

    [L831-031] - Add the firts tests
    *AdaPluginTest.java:
      (testGetExtensions): test that there is exactly the expected number
                           of extensions.
    *AdaColorizerTest.java
      (testGetTokenizers): test that there is exactly the expected nubmer
                           of tokenizers.
    
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/qualimetrics@194429 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ sonar-ada-plugin/src/test/java/org/sonar/plugins/ada/AdaColorizerTest.java
@@ -0,0 +1,24 @@
+/*
+ * Sonar Ada Plugin
+ * Copyright (C) 2012, AdaCore
+ */
+package org.sonar.plugins.ada;
+
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import org.sonar.colorizer.Tokenizer;
+
+public class AdaColorizerTest {
+
+    /**
+     * Test of getTokenizers method, of class AdaColorizer.
+     */
+    @Test
+    public void testGetTokenizers() {
+        List<Tokenizer> tokenizer = (new AdaColorizer()).getTokenizers();
+        assert(tokenizer != null);
+        assertEquals(3, tokenizer.size());
+    }
+}
---
+++ sonar-ada-plugin/src/test/java/org/sonar/plugins/ada/AdaPluginTest.java
@@ -0,0 +1,20 @@
+/*
+ * Sonar Ada Plugin
+ * Copyright (C) 2012, AdaCore
+ */
+package org.sonar.plugins.ada;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AdaPluginTest {
+
+    /**
+     * Test of getExtensions method, of class AdaPlugin.
+     */
+    @Test
+    public void testGetExtensions() {
+        AdaPlugin plugin = new AdaPlugin();
+        assertEquals(9, plugin.getExtensions().size());
+    }
+}
