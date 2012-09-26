2fe9709bd403c2aee73769e2b45140291416b9b1
martin <martin@936e1b1b-40f2-da11-902a-00137254ae57>
2012-09-26 09:02:21 +0000

    [L919-021] - Fix bug in the script: description was tuncated.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/qualimetrics@195577 936e1b1b-40f2-da11-902a-00137254ae57

--- sonar-ada-plugin/src/tools/codepeer_create_resources.py
+++ sonar-ada-plugin/src/tools/codepeer_create_resources.py
@@ -14,18 +14,31 @@ import os
 #############
 
 REPOSITORY_KEY = 'codepeer'
-DOC_FILENAME='messages_and_annotations.txt'
+DOC_FILENAME='messages_and_annotations.rst'
 COMMENT = '#### Codepeer rule repository generated from html doc page: messages_and_annotations ####'
 
 
 ## CPDocParser #######################################################################
 ##
 class CPDocParser(object):
+    """Represent parser for codepeer documentation
 
+    Constructor takes the path to the documention
+    as parameter.
+    """
     def __init__(self, doc_path):
         self.doc_path = doc_path
 
     def parse_doc(self, rule_repo):
+        """Parse codepeer documentation
+
+        Parse documentation in the given location. Some details about the algo:
+        recording: assigned to True when the line match the beggining of the
+                   rule table.
+                   assigned to False when line is \n,identified the end of table
+        started: assigned to True when starts saving info of the first line of a
+                 rules table line.
+        """
         with open (os.path.join(self.doc_path,DOC_FILENAME), 'rt') as doc:
             recording = False
             started = False
@@ -42,18 +55,20 @@ class CPDocParser(object):
                 if beginning.match(line):
                     recording = True
 
-                # When there is a multiple line for a table
-                if recording and started and line.startswith('|'):
-                    infos = line.split('|')
-                    rule.key += ' ' + infos[1].strip()
-                    rule.description += ' ' + infos[2].strip()
-
                 # Beginning of a table line
                 if recording and not started and line.startswith('|'):
                     infos = line.split('|')
                     rule.key = infos[1].strip()
                     rule.description = infos[2].strip()
                     started = True
+                    continue
+
+                # When there is a multiple line for a table
+                if recording and started and line.startswith('|'):
+                    print line
+                    infos = line.split('|')
+                    rule.key += ' ' + infos[1].strip()
+                    rule.description += ' ' + infos[2].strip()
 
                 # End of a table line
                 if recording and end_rule.match(line):
@@ -67,11 +82,14 @@ class CPDocParser(object):
 ## _parse_command_line #######################################################################
 ##
 def _parse_command_line():
-    """Parse the command line"""
+    """Parse the command line
+
+    Retrieve path to rst codepeer user guide documentation
+    """
     parser = argparse.ArgumentParser(description=
                                        'Codepeer rule repository generator')
     parser.add_argument('--doc=', action='store', dest='doc_path', type=str,
-                         help='Absolute path to html codepeer documentation, usally: $GNAT_HOME/share/doc/codepeer/user_guide/html/',
+                         help='Absolute path to rst codepeer user guide documentation',
                          required=True)
     return parser.parse_args()
 
@@ -79,7 +97,12 @@ def _parse_command_line():
 ## Script entry point #######################################################################
 ##
 def __main__():
-    """Script entry point"""
+    """Script entry point
+
+    Creates rule repository and append the defalut profile with the rules.
+    This entry point must be outsourced to drive the generation of all rule
+    repository (for every integrated tool).
+    """
     rule_exporter = RuleRepositoryExporter()
     profile_exporter = ProfileExporter()
     rule_repo = RuleRepository(REPOSITORY_KEY, COMMENT)
