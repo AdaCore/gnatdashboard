0b075a2f31afba75b054a0f89a08114e613b6165
martin <martin@936e1b1b-40f2-da11-902a-00137254ae57>
2012-11-28 17:44:39 +0000

    Refactor
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/qualimetrics@198822 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ sonar-ada-plugin/src/main/java/org/sonar/plugins/ada/codepeer/CodePeerRuleCategory.java
@@ -0,0 +1,10 @@
+/*
+ * Sonar Ada Plugin
+ * Copyright (C) 2012, AdaCore
+ */
+
+package org.sonar.plugins.ada.codepeer;
+
+public enum CodePeerRuleCategory {
+    CHECK, WARNING, RACE_CONDITION, INFORMATIONAL
+}
