05b0d19c6f0c39561e26d4095563ade07c144fdc
setton <setton@936e1b1b-40f2-da11-902a-00137254ae57>
2013-06-12 09:53:17 +0000

    Use GNATCOLL.Traces rather than Text_IO. Add error messages for various  failure cases.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/qualimetrics@211304 936e1b1b-40f2-da11-902a-00137254ae57

--- qualimetrics/src/qualimetrics.adb
+++ qualimetrics/src/qualimetrics.adb
@@ -5,8 +5,6 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 
-with Ada.Text_IO;           use Ada.Text_IO;
-
 with GNATCOLL.Projects;     use GNATCOLL.Projects;
 with GNATCOLL.VFS;          use GNATCOLL.VFS;
 
@@ -17,9 +15,16 @@ with Utils;
 with Database_Interface;    use Database_Interface;
 with Qmt_Command_Line;      use Qmt_Command_Line;
 with Project_Parser;        use Project_Parser;
---  with GNATCOLL.Traces;
+
+with GNATCOLL.Traces;       use GNATCOLL.Traces;
 
 procedure Qualimetrics is
+
+   Main_Trace : constant Trace_Handle :=
+     GNATCOLL.Traces.Create ("QUALIMETRICS.MAIN", Off);
+
+   Error_Trace : constant Trace_Handle :=
+     GNATCOLL.Traces.Create ("QUALIMETRICS.Errors", On);
 
    procedure Main;
    --  Program entry point
@@ -35,7 +40,7 @@ procedure Qualimetrics is
       Deposit_Dir  : Virtual_File;
       Command_Line : Qualimetrics_Command_Line;
    begin
-      --  GNATCOLL.Traces.Parse_Config_File (".gnatdebug");
+      GNATCOLL.Traces.Parse_Config_File;
 
       GPS.CLI_Utils.Create_Kernel_Context (Kernel);
       Command_Line.Configure;
@@ -43,6 +48,7 @@ procedure Qualimetrics is
       --  Error output messages are handled by Parse function, according
       --  to the case of failure.
       if not Command_Line.Parse (Kernel) then
+         Trace (Error_Trace, "Could not parse the command line", Red_Fg);
          return;
       end if;
 
@@ -50,6 +56,7 @@ procedure Qualimetrics is
       if not Load_Project_Tree (Command_Line.Get_Project_Name,
                                 Kernel)
       then
+         Trace (Error_Trace, "Could not load the project", Red_Fg);
          return;
       end if;
 
@@ -59,16 +66,19 @@ procedure Qualimetrics is
 
       --  Create Database
       if not Initialize_DB (Deposit_Dir) then
+         Trace (Error_Trace, "Could not initialize the database", Red_Fg);
          return;
       end if;
-      Put_Line ("DB Created");
+
+      Trace (Main_Trace, "DB Created");
 
       --  Save project tree to DB
       Save_Project_Tree (Kernel.Registry.Tree.Root_Project);
 
       GPS.CLI_Utils.Destroy_Kernel_Context (Kernel);
       Command_Line.Destroy;
-      --        GNATCOLL.Traces.Finalize;
+
+      GNATCOLL.Traces.Finalize;
    end Main;
 
 begin
