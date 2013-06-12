c960a8fe72eab200c47b6527971183f3f81f1a9c
martin <martin@936e1b1b-40f2-da11-902a-00137254ae57>
2013-06-12 12:33:53 +0000

    Pyhton module that provides API for qualimetrics plugins.
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/qualimetrics@211318 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ qualimetrics/share/qualimetrics/core/qualimetrics-api.py
@@ -0,0 +1,45 @@
+#########################################
+#        Qualimetrics Plugin API        #
+#########################################
+
+
+## OutputParserMetaClass ######################################################
+class OutputParserMetaClass(type):
+    registered = dict()   # list of registered parsers
+
+    def __new__(cls, name, bases, attrs):
+        new_class = type.__new__(cls, name, bases, attrs)
+        OutputParserMetaClass.registered[new_class.get_name()] = new_class
+        return new_class
+
+    def get_name(self):
+        """Return the name of the parser, either from a "name" class
+            attribute, or from the class name
+        """
+        return getattr(self, 'name', self.__name__).lower()
+
+## OutputParser #####################################################xxxxxxxxx#
+class OutputParser(object):
+    __metaclass__ = OutputParserMetaClass
+
+    def __init__(self,child):
+        self.child = child
+
+    def on_stdout(self,text):
+        if self.child != None:
+            self.child.on_stdout (text)
+
+    def on_stderr(self,text):
+        if self.child != None:
+           self.child.on_stderr (text)
+
+    def on_exit(self,status=0):
+        if self.child != None:
+            self.child.on_exit (status)
+
+def create_parser(name, child=None):
+    if OutputParserMetaClass.registered.has_key (name):
+        return OutputParserMetaClass.registered[name](child)
+    else:
+        return None
+
