0d1bbee4f85b0372ca79a368e477e1c9e3a2765c
JC Delay <delay@adacore.com>
2014-01-13 17:26:01 +0100

    Tentative fix for GNATmetric test.
    The testcase tries to call sqlite3 command-line tool which is not
    available by default on all machines/platforms. Since we don't want to
    maintain an install of this tool nor handle it as part of GNAThub's
    dependencies, this patch implements a Python wrapper on top of
    SQLAlchemy that can query a SQLite database to replace the need for
    sqlite3.
    
    t/tests/gnatmetric/ChangeLog:
    
     * gnathub_query.py: new file to replace sqlite3.
     * test.sh: update the test file to use gnathub_query.py instead of
       sqlite3.
    
    Change-Id: I94330671cb3216b8a8508ee72b3344d43d1e5090
    TN: N113-034

--- gnathub/t/tests/gnatmetric/test.sh
+++ gnathub/t/tests/gnatmetric/test.sh
@@ -1,15 +1,20 @@
-# launch gnathub on a trivial project
+# Fetch our own version of Python (which has SQLAlchemy installed)
+BASE="$(dirname $(dirname `which gnathub`))"
+PYTHON="$BASE/share/gnathub/python/bin/python"
+
+# Launch gnathub on a trivial project
 gnathub -q -P default --plugins gnatmetric
 
 tool_name="GNATmetric"
 
-# verify that gnatmetric has saved metric in db
-gm_rules=`sqlite3 obj/gnathub/gnathub.db \
+# Verify that gnatmetric has saved metric in db
+GNATmetric_count=`"$PYTHON" gnathub_query.py obj/gnathub/gnathub.db \
     "select count(r.id) from rules r, tools t where t.id = r.tool_id and t.name = \"$tool_name\";"`
 
-if [ "$gm_rules" -lt "1" ]; then
+if [ $((GNATmetric_count)) -lt 1 ]; then
     echo "No rule saved for GNATmetric"
 fi
 
-# verify that gnatmetric tool has been saved in db
-sqlite3 obj/gnathub/gnathub.db "select name from tools;" | grep "$tool_name"
+# Verify that gnatmetric tool has been saved in db
+"$PYTHON" gnathub_query.py \
+    obj/gnathub/gnathub.db "select name from tools;" | grep "$tool_name"
