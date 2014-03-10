13c2adb0c6e2d55822e90319e439ca0f44136048
Nicolas Setton <setton@adacore.com>
2014-03-10 14:12:21 -0400

    Rewrite, for Windows support and without SQLalchemy
    Change-Id: I6966615097b7a51c632e07e8b14e52a057f720e6

--- gnathub/t/tests/tools/GNATmetric/test.sh
+++ gnathub/t/tests/tools/GNATmetric/test.sh
@@ -1,20 +1,2 @@
-# Fetch our own version of Python (which has SQLAlchemy installed)
-BASE="$(dirname $(dirname `which gnathub`))"
-PYTHON="$BASE/share/gnathub/python/bin/python"
-
-# Launch gnathub on a trivial project
 gnathub -q -P default --plugins gnatmetric
-
-tool_name="GNATmetric"
-
-# Verify that gnatmetric has saved metric in db
-GNATmetric_count=`"$PYTHON" gnathub_query.py obj/gnathub/gnathub.db \
-    "select count(r.id) from rules r, tools t where t.id = r.tool_id and t.name = \"$tool_name\";"`
-
-if [ $((GNATmetric_count)) -lt 1 ]; then
-    echo "No rule saved for GNATmetric"
-fi
-
-# Verify that gnatmetric tool has been saved in db
-"$PYTHON" gnathub_query.py \
-    obj/gnathub/gnathub.db "select name from tools;" | grep "$tool_name"
+gnathub -q -Pdefault --exec script.py
