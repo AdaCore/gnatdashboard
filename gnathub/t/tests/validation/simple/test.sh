835f3752ea78292ff3dc1f34e05d7deb35bf9c13
Nicolas Setton <setton@adacore.com>
2014-03-10 13:59:02 -0400

    Make test compatible with Windows.
    Change-Id: Id07220fedf080f4edc2f7dcb1f34a32b2d09855f

--- gnathub/t/tests/validation/simple/test.sh
+++ gnathub/t/tests/validation/simple/test.sh
@@ -1,6 +1,3 @@
-# launch GNAThub on a trivial project
 gnathub -q -P default --plugins gnatmetric,gnatcheck,sonarconfig
 
-# verify that the object directory contains the expected files
-# at the expected locations
 python check-output.py
