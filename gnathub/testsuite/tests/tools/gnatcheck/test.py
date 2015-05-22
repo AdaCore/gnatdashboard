d14ad9482b14cba49d69c0e19a57e6ea7663d2ab
JC Delay <delay@adacore.com>
2015-05-22 14:58:54 +0200

    Add a testcase for GNATcheck tool
    Change-Id: I27a19e7122331b333cd1bc87993fe8e49142db00
    TN: O522-001

---
+++ gnathub/testsuite/tests/tools/gnatcheck/test.py
@@ -0,0 +1,37 @@
+"""Check that all messages have been reported."""
+
+import os
+
+from ConfigParser import SafeConfigParser
+
+from unittest import TestCase
+from support import GNAThub, Project, Script
+
+
+COMPILER_WARNING = 'f.adb 10:7'
+IMPROPER_RETURNS = 'simple.adb 33:17'
+SECTIONS = sorted(['simple.adb', 'f.adb', 'f.ads',
+                   COMPILER_WARNING, IMPROPER_RETURNS])
+
+
+class TestGNATcheckSupport(TestCase):
+    def setUp(self):
+        self.longMessage = True
+        self.gnathub = GNAThub(Project.simple(), plugins=['gnatcheck'])
+
+    def testDatabaseContent(self):
+        script_output_file = os.path.abspath('script.out')
+        self.gnathub.run(script=Script.db2cfg(), output=script_output_file)
+
+        parser = SafeConfigParser()
+        parser.optionxform = str
+
+        parser.read(script_output_file)
+        self.assertListEqual(sorted(parser.sections()), SECTIONS)
+
+        self.assertTrue(
+            parser.has_option(COMPILER_WARNING, 'Warnings'),
+            'missing "Warnings" entry')
+        self.assertTrue(
+            parser.has_option(IMPROPER_RETURNS, "Improper_Returns"),
+            'missing "Improper_Returns" entry')
