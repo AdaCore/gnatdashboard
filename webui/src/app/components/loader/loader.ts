ef8a40aeb9d04856d4880dd2c48eaf6e118de48b
Charly Delay <delay@adacore.com>
2016-03-07 18:45:40 +0100

    New loader component
    Change-Id: I9c749df38375fed0395084c2b06b99e66b54a932

---
+++ webui/src/app/components/loader/loader.ts
@@ -0,0 +1,8 @@
+import { Component } from "angular2/core";
+import { CORE_DIRECTIVES } from "angular2/common";
+
+@Component({
+    selector: "loader",
+    template: "<div class=\"loader\">Loading...</div>"
+})
+export class Loader { }
