4525543df7bb1c6840a9df5d4f0dc9e9b9c8026a
Charly Delay <delay@adacore.com>
2016-08-09 15:57:06 -0400

    chore(webui): define Array.sum()
    Change-Id: Ib76bbbc12bcf73367e761366f7d01d819e90860e

---
+++ webui/src/app/array-utils.ts
@@ -0,0 +1,9 @@
+interface Array<T> {
+  sum: (exp?: (item: any) => number) => number;
+}
+
+Array.prototype.sum = function(exp?: (item: any) => number): number {
+    return this
+        .map(exp || ((item) => item))
+        .reduce((count, val) => count + val, 0 /* initialValue */);
+};
