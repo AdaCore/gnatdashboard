d2958aab155bed8689729ec6b6b78a0c93c853a2
Charly Delay <delay@adacore.com>
2016-08-12 11:46:16 -0400

    feat(webui): display list of sources in Project
    Change-Id: Ibc41ceb90d90833056aaf3bd6802c8e269752c63

--- webui/src/app/project/project.component.ts
+++ webui/src/app/project/project.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CORE_DIRECTIVES } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
 
 import { GNAThubService } from '../gnathub.service';
 import { IGNAThubReport } from 'gnat';
@@ -20,17 +22,27 @@ import { MissingReportError } from '../errors';
 })
 export class Project {
     private project: string = null;
+    private directory: string = null;
     private report: IGNAThubReport = null;
     private isReportFetchError: boolean = false;
+    private sub: Subscription = null;
 
     constructor(
         private gnathub: GNAThubService,
-        private route: ActivatedRoute) {}
+        private route: ActivatedRoute,
+        private router: Router) {}
 
     ngOnInit(): void {
         this.project = this.route.snapshot.params['name'];
+        this.sub = this.router.routerState.queryParams.subscribe(params => {
+            this.directory = params['directory'];
+        });
         this.gnathub.getReport().subscribe(
             report => this.report = report,
             error => this.isReportFetchError = !!error);
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 }
