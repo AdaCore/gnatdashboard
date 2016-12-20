ea345b6adca2ac7a79bd5e45771f3df2ce4ab855
Charly Delay <delay@adacore.com>
2016-12-20 12:58:52 -0500

    chore(gnathub): adjust copyrights
    Change-Id: I170a5541bdd08546b59b2cfafe41e0abf87b9682

--- gnathub/src/gnathub/gnathub-constants.adb
+++ gnathub/src/gnathub/gnathub-constants.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                               G N A T h u b                              --
 --                                                                          --
---                     Copyright (C) 2013-2015, AdaCore                     --
+--                     Copyright (C) 2013-2016, AdaCore                     --
 --                                                                          --
 -- This is free software;  you can redistribute it  and/or modify it  under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
