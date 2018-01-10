f057055f1db1ead89d819ffa030c3f2ec55bbfe8
Nicolas Setton <setton@adacore.com>
2018-01-10 18:31:58 -0500

    Generate gnatcheck.xml rules from the output of gnatcheck -h
    Add a cheapo script to do that and a Makefile entry.
    
    For R109-055.
    
    Change-Id: I37b801253bccfda2131ed2b57f580b07ef20c644

---
+++ sonar-ada/sonar-ada-plugin/src/ruledef/scripts/generate_gnatcheck_rules.py
@@ -0,0 +1,63 @@
+"""Generate the contents of the gnatcheck rules file.
+
+This script takes as input the output of gnatcheck -h
+"""
+
+import sys
+
+# Print the header
+print """<?xml version='1.0' encoding='UTF-8'?>
+<rules>
+"""
+
+# The output of gnatcheck -h looks like this:
+# gnatcheck currently implements the following rules:
+#  Abstract_Type_Declarations - abstract types
+#  Annotated_Comments - use of comment annotations
+#  Anonymous_Arrays - anonymous array types
+#  Anonymous_Subtypes - anonymous subtypes
+#  Binary_Case_Statements - CASE statements that can be replaced with IF
+#   statements
+#  (...)
+#  gnatcheck allows activation of the following checks provided by GNAT
+#  using the same syntax to control these checks as for other rules:
+#   Warnings     - compiler warnings
+#   Style_Checks - compiler style checks
+#   Restrictions - checks made by pragma Restriction_Warnings
+
+current_rule_text = ""
+current_rule_label = ""
+
+
+def print_current_rule():
+    """Print the current rule"""
+    if current_rule_text:
+        print """    <rule>
+      <key>{}</key>
+      <name>{}</name>
+      <description>{}</description>
+      <tag>gnatcheck</tag>
+    </rule>""".format(current_rule_label.lower(),
+                      current_rule_text,
+                      current_rule_text)
+
+
+for j in sys.stdin.readlines():
+    if j.startswith("gnatcheck allows activation"):
+        # This is the terminator for us
+        print_current_rule()
+        break
+    if j.startswith("gnatcheck"):
+        # Ignore
+        continue
+    if j.startswith("  "):
+        # This is the continuation of the previous rule
+        current_rule_text += " " + j.strip()
+    else:
+        # This is the definition of a rule
+        print_current_rule()
+        current_rule_label, current_rule_text = j.strip().split(" - ", 1)
+
+
+# Print the footer
+print "</rules>"
