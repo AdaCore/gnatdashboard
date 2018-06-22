964005f29ce77a678276aa8915e90ac171b2d27e
Adrien Boulanger <boulanger@adacore.com>
2018-06-22 15:15:56 +0200

    R131-030: Create the gnatstack plugin
    Also add a test
    
    Change-Id: Ie5dea719b7b6d4b73eb8ea8aff072185d226d92e

---
+++ gnathub/share/gnathub/core/gnatstack.py
@@ -0,0 +1,302 @@
+# GNAThub (GNATdashboard)
+# Copyright (C) 2013-2018, AdaCore
+#
+# This is free software;  you can redistribute it  and/or modify it  under
+# terms of the  GNU General Public License as published  by the Free Soft-
+# ware  Foundation;  either version 3,  or (at your option) any later ver-
+# sion.  This software is distributed in the hope  that it will be useful,
+# but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN-
+# TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for  more details.  You should have  received  a copy of the GNU
+# General  Public  License  distributed  with  this  software;   see  file
+# COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy
+# of the license.
+
+"""GNAThub plug-in for the GNATstack command-line tool.
+
+It exports the GNATstack class which implements the :class:`GNAThub.Plugin`
+interface. This allows GNAThub's plug-in scanner to automatically find this
+module and load it as part of the GNAThub default execution.
+"""
+
+import os
+
+import GNAThub
+from GNAThub import Plugin, Reporter, Runner
+
+from xml.etree import ElementTree
+from xml.etree.ElementTree import ParseError
+
+
+class GNATstack(Plugin, Runner, Reporter):
+    """GNATstack plugin for GNAThub."""
+
+    # GNATstack exits with an error code of 1 even on a successful run
+    VALID_EXIT_CODES = (0, 1)
+    RANKING = GNAThub.RANKING_UNSPECIFIED
+
+    def __init__(self):
+        super(GNATstack, self).__init__()
+
+        self.tool = None
+        self.output = os.path.join(GNAThub.Project.object_dir(),
+                                   "stack_usage.xml")
+        self.resources = {}
+
+        # Map of ID => entity
+        self.subprograms = {}
+
+    def __cmd_line(self):
+        """Create GNATstack command line arguments list.
+
+        :return: the GNATstack command line
+        :rtype: collections.Iterable[str]
+        """
+
+        cmd_line = [
+            'gnatstack', '-Q', '-x', '-Wa',
+            '-P', GNAThub.Project.path()] + GNAThub.Project.scenario_switches()
+        if GNAThub.Project.target():
+            cmd_line[0] = '{}-{}'.format(GNAThub.Project.target(), cmd_line[0])
+        if GNAThub.Project.runtime():
+            cmd_line.extend(('--RTS', GNAThub.Project.runtime()))
+        if GNAThub.subdirs():
+            cmd_line.extend(['--subdirs=' + GNAThub.subdirs()])
+        return cmd_line
+
+    def run(self):
+        """Execute GNATstack.
+
+        Returns according to the success of the execution of the tool:
+
+            * ``GNAThub.EXEC_SUCCESS``: on successful execution
+            * ``GNAThub.EXEC_FAILURE``: on any error
+        """
+
+        return GNAThub.EXEC_SUCCESS if GNAThub.Run(
+            self.name, self.__cmd_line()
+        ).status in GNATstack.VALID_EXIT_CODES else GNAThub.EXEC_FAILURE
+
+    def report(self):
+        """Parse GNATstack output file report.
+
+        Returns according to the success of the analysis:
+
+            * ``GNAThub.EXEC_SUCCESS``: on successful execution and analysis
+            * ``GNAThub.EXEC_FAILURE``: on any error
+        """
+        self.log.info('clear tool references in the database')
+        GNAThub.Tool.clear_references(self.name)
+
+        self.info('analyse report')
+
+        self.tool = GNAThub.Tool(self.name)
+        if not os.path.exists(self.output):
+            self.error('no report found')
+            return GNAThub.EXEC_FAILURE
+        self.log.debug('parse XML report: %s', self.output)
+
+        # List of resource messages suitable for tool level bulk insertion
+        resources_messages = []
+        # Map of list of messages by entities
+        entities_messages_map = {}
+        # List of entity messages suitable for tool level bulk insertion
+        entities_messages = []
+        # List of skipped file
+        skipped = []
+
+        try:
+            tree = ElementTree.parse(self.output)
+            global_node = tree.find('./global')
+
+            # Retrieve the metrics and the map of subprogram by id
+            subprograms = tree.find('./subprogramset').findall('./subprogram')
+            if subprograms:
+                unknown_global = GNAThub.Rule("Unknown Global Stack Usage",
+                                              "Unknown Global Stack Usage",
+                                              GNAThub.METRIC_KIND,
+                                              self.tool)
+                static_global = GNAThub.Rule("Static Global Stack Usage",
+                                             "Static Global Stack Usage",
+                                             GNAThub.METRIC_KIND,
+                                             self.tool)
+                unknown_local = GNAThub.Rule("Unknown Local Stack Usage",
+                                             "Unknown Local Stack Usage",
+                                             GNAThub.METRIC_KIND,
+                                             self.tool)
+                static_local = GNAThub.Rule("Static Local Stack Usage",
+                                            "Static Local Stack Usage",
+                                            GNAThub.METRIC_KIND,
+                                            self.tool)
+            for node in subprograms:
+                subprogram_id = node.attrib.get('id')
+                locations = node.find('./locationset').findall('./location')
+                global_usage = node.find('./globalstackusage')
+                local_usage = node.find('./localstackusage')
+                name = node.attrib.get('prefixname')
+                if name == "indirect call":
+                    continue
+
+                for loc in locations:
+                    file = loc.attrib.get('file')
+                    line = loc.attrib.get('line')
+                    column = loc.attrib.get('column')
+                    if name == "Debug_Test":
+                        self.info(file + " at " + line + ":" + column)
+                    if file in self.resources:
+                        resource = self.resources[file]
+                    else:
+                        resource = GNAThub.Resource(file, GNAThub.FILE_KIND)
+                        self.resources[file] = resource
+                    entity = GNAThub.Entity(name, int(line), int(column),
+                                            int(column), resource)
+                    # Only link the id to the first location of the entity
+                    if subprogram_id not in self.subprograms:
+                        self.subprograms[subprogram_id] = entity
+                    else:
+                        continue
+
+                    size = global_usage.attrib.get('size')
+                    if global_usage.attrib.get('qualifier') == "UNKNOWN":
+                        metric = unknown_global
+                    else:
+                        metric = static_global
+                    global_metric = GNAThub.Message(metric, size)
+
+                    size = local_usage.attrib.get('size')
+                    if local_usage.attrib.get('qualifier') == "UNKNOWN":
+                        metric = unknown_local
+                    else:
+                        metric = static_local
+                    local_metric = GNAThub.Message(metric, size)
+
+                    entities_messages_map[subprogram_id] = (
+                        [global_metric, local_metric])
+
+            # Analyse the indirect calls
+            indirects = global_node.find('./indirectset').findall('./indirect')
+            if indirects:
+                indirect_rule = GNAThub.Rule("Indirect Call",
+                                             "Indirect Call",
+                                             GNAThub.RULE_KIND,
+                                             self.tool)
+            for node in indirects:
+                indirect_id = node.attrib.get('id')
+                if indirect_id not in self.subprograms:
+                    continue
+
+                set = node.find('./indirectcallset').findall('./indirectcall')
+                for call in set:
+                    line = call.find('./line').find('./value').text
+                    message = GNAThub.Message(indirect_rule, 'indirect call')
+                    entities_messages_map[indirect_id].append(message)
+
+            # Analyse the external calls
+            externals = global_node.find('./externalset').findall('./external')
+            if externals:
+                external_rule = GNAThub.Rule("External Call",
+                                             "External Call",
+                                             GNAThub.RULE_KIND,
+                                             self.tool)
+            for node in externals:
+                subprogram_id = node.attrib.get('id')
+                if subprogram_id not in self.subprograms:
+                    continue
+                message = GNAThub.Message(external_rule, "external call")
+                entities_messages_map[subprogram_id].append(message)
+
+            # Analyse the potential cycle
+            cycles = global_node.find('./cycleset').findall('./cycle')
+            if cycles:
+                cycle_rule = GNAThub.Rule("Potential Cycle",
+                                          "Potential Cycle",
+                                          GNAThub.RULE_KIND,
+                                          self.tool)
+            for node in cycles:
+                cycle_subprograms = node.findall('./subprogram')
+                cycle_list = []
+                for sub in cycle_subprograms:
+                    subprogram_id = sub.attrib.get('id')
+                    cycle_list.append(self.subprograms[subprogram_id].name)
+                subprogram_id = cycle_subprograms[0].attrib.get('id')
+                cycle_list.append(self.subprograms[subprogram_id].name)
+                message = GNAThub.Message(cycle_rule,
+                                          "potential cycle detected:\n\t\t" +
+                                          "\n\t\t".join(cycle_list))
+                entities_messages_map[subprogram_id].append(message)
+
+            # Analyse the unbounded frames
+            unboundeds = (
+                global_node.find('./unboundedset').findall('./unbounded'))
+            if unboundeds:
+                unbounded_rule = GNAThub.Rule("Unbounded Frame",
+                                              "Unbounded Frame",
+                                              GNAThub.RULE_KIND,
+                                              self.tool)
+            for node in unboundeds:
+                subprogram_id = node.attrib.get('id')
+                if subprogram_id in self.subprograms:
+                    message = GNAThub.Message(unbounded_rule,
+                                              "This frame is unbounded")
+                    entities_messages_map[subprogram_id].append(message)
+
+            # Analyse the entry points
+            entries = tree.find('./entryset').findall('./entry')
+            # There is always an entry, so create the rule anyway
+            entry_rule = GNAThub.Rule("Entry point",
+                                      "Entry point",
+                                      GNAThub.RULE_KIND,
+                                      self.tool)
+            for node in entries:
+                subprogram_id = node.attrib.get('id')
+
+                if subprogram_id not in self.subprograms:
+                    continue
+                entity = self.subprograms[subprogram_id]
+                local_stack = node.find('./localstackusage')
+                size = local_stack.attrib.get('size')
+                qualifier = local_stack.attrib.get('qualifier')
+
+                if qualifier == "UNKNOWN":
+                    text = "The estimated"
+                else:
+                    text = "The"
+                text += (' call stack size for the entry point "%s" is %s' %
+                         (entity.name, str(size)))
+
+                callchain_list = []
+                for sub in node.find('./callchain').findall('./subprogram'):
+                    chain_id = sub.attrib.get('id')
+                    callchain_list.append(self.subprograms[chain_id].name)
+                text += (" and the callchain is:\n\t\t%s" %
+                         "\n\t\t".join(callchain_list))
+                message = GNAThub.Message(entry_rule, text)
+                entities_messages_map[subprogram_id].append(message)
+
+            # Project message explaining the accuracy of the metrics
+            accurate = global_node.find('./accurate')
+            if accurate.find('./value').text == "FALSE":
+                project = GNAThub.Resource(GNAThub.Project.name(),
+                                           GNAThub.PROJECT_KIND)
+                rule = GNAThub.Rule("Accuracy", "Accuracy",
+                                    GNAThub.RULE_KIND, self.tool)
+                text = ("worst case may not be accurate because of: " +
+                        ("indirect calls/" if indirects else "") +
+                        ("cycles/" if cycles else "") +
+                        ("unbounded frames/" if unboundeds else "") +
+                        ("external calls" if externals else ""))
+                text = text[:-1] if text[-1] == '/' else text
+                resources_messages.append([project,
+                                           [[GNAThub.Message(rule, text),
+                                             0, 1, 1]]])
+
+            # Insert the messages and the metrics
+            for key, value in entities_messages_map.iteritems():
+                entities_messages.append([self.subprograms[key], value])
+            self.tool.add_messages(resources_messages, entities_messages)
+        except ParseError as why:
+            self.log.exception('failed to parse XML report')
+            self.error('%s (%s:%s)' % (why, why.filename, why.lineno))
+            return GNAThub.EXEC_FAILURE
+        else:
+            return GNAThub.EXEC_SUCCESS
---
+++ gnathub/testsuite/tests/tools/gnatstack/test.py
@@ -0,0 +1,73 @@
+"""Check that all files have been created."""
+
+import os
+
+from ConfigParser import SafeConfigParser
+
+from unittest import TestCase
+from support.mock import GNAThub, Project, Script
+
+TEXT_IO = ["a-textio.ads 263:14"]
+F_ADB = ["f.adb", "f.adb 9:4", "f.adb 21:1"]
+F_ADS = ["f.ads"]
+SIMPLE_ADB = ["simple.adb", "simple.adb 19:4", "simple.adb 28:4",
+              "simple.adb 10:4", "simple.adb 51:5"]
+SIMPLE_4_1 = ["simple.adb 4:1"]
+B_SIMPLE_ADB = ["b__simple.adb 209:4"]
+SECTIONS = TEXT_IO + F_ADB + F_ADS + SIMPLE_ADB + SIMPLE_4_1 + B_SIMPLE_ADB
+
+EXTERNAL_CALL = [
+    "External Call", "external call"
+]
+
+ENTRY_POINT = [
+    "Entry point", ('The estimated call stack size for the entry ' +
+                    'point "Main" is 352 and the callchain is:' +
+                    '\nMain\nTest_Case\nFoo\nImage_Integer')
+]
+
+METRICS_SIMPLE_4_1 = {
+    "Unknown Global Stack Usage": 288,
+    "Static Local Stack Usage": 176
+}
+
+
+class TestGNATstackSupport(TestCase):
+    def setUp(self):
+        self.longMessage = True
+        self.project = Project.simple()
+        self.project.build()
+        self.project.run()
+        self.gnathub = GNAThub(self.project, plugins=['gnatstack'])
+
+    def testDatabaseContent(self):
+        script_output_file = os.path.abspath('script.out')
+        self.gnathub.run(script=Script.db2cfg(), output=script_output_file)
+
+        parser = SafeConfigParser()
+        parser.optionxform = str
+
+        parser.read(script_output_file)
+        sections = parser.sections()
+        for s in SECTIONS:
+            self.assertTrue(s in sections, "Missing section: " + s)
+
+        self.assertTrue(parser.has_option(TEXT_IO[0], EXTERNAL_CALL[0]),
+                        "Should be an external call")
+        self.assertEqual(parser.get(TEXT_IO[0], EXTERNAL_CALL[0]),
+                         EXTERNAL_CALL[1],
+                         'Wrong message')
+
+        self.assertTrue(parser.has_option(B_SIMPLE_ADB[0], ENTRY_POINT[0]),
+                        "Entry point not found")
+        self.assertEqual(parser.get(B_SIMPLE_ADB[0], ENTRY_POINT[0]),
+                         ENTRY_POINT[1],
+                         'unexpected value for the entry point')
+
+        for metric, value in METRICS_SIMPLE_4_1.iteritems():
+            self.assertTrue(
+                parser.has_option(SIMPLE_4_1[0], metric),
+                'missing entry for "%s"' % metric)
+            self.assertEqual(
+                parser.getfloat(SIMPLE_4_1[0], metric), value,
+                'unexpected value for "%s"' % metric)
