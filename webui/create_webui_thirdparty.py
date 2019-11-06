15513ab83230a3f3ed266ba7abc2b739e41fce40
Lespinasse <lespinasse@adacore.com>
2019-11-06 17:58:15 +0100

    WebUI: Automize thirdparty creation
    	- Add a python script to create thirdparty with date
    	- Add new rule in makefile "make thirdparty" to launch script
    
    Change-Id: I9ae0acaefe4b4ca36c0be3a38c1587379d7e8ac0
    For: S920-024

---
+++ webui/create_webui_thirdparty.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import time
+from subprocess import call
+
+DATETIME = time.strftime('%Y-%m-%d')
+NODE_MODULE_PATH = './node_modules/'
+TAR_NAME = 'webui_node_modules_' + DATETIME + '.tar.gz '
+
+TAR_CMD = "tar -zcf " + TAR_NAME + NODE_MODULE_PATH
+print "Executing : " + TAR_CMD
+print "Can take a little bit of time..."
+call(TAR_CMD.split(' '))
